7274bff5ac024050514932a8d96b1471ad3ec1d3
DrDarka90 <DrDarka90@users.noreply.github.com>
2019-04-12 20:06:05 -0400

    Add files via upload
    Version Final

--- Programas/Control_Monitoreo/enviar_datos_ubi.py
+++ Programas/Control_Monitoreo/enviar_datos_ubi.py
@@ -3,12 +3,11 @@
 import datetime
 import time
 from time import strftime
+from time import clock
 import requests
-import random
 import Adafruit_ADS1x15
 import RPi.GPIO as GPIO
 import lcddriver
-import requests
 
 
 #Tokens para el envio de informacion a Ubidots
@@ -36,16 +35,18 @@ GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+#Pin de conexion para el rele
 GPIO.setup(4, GPIO.OUT)
 
+# variables para la medicion del caudal
 contador1 = 0
 ISRcontador1 = 0
 contador2 = 0
 ISRcontador2 = 0
 caudal1 = 0
 caudal2 = 0
-a = 0
-b = 0
+tiempo1 = 0
+
 
 # Variable de estado del Rele
 
@@ -77,18 +78,19 @@ lcd.lcd_clear()
 def build_payload(variable_1, variable_2, variable_3, variable_4, variable_5):
     # Crea  el archivo log para el registro de las mediciones
     with open("/home/pi/Documents/Programas/Control_Monitoreo/Registro_Sistema.csv","a") as log:
-        global tiempo
-        global estado
-        tiempo = time.strftime("%H:%M")
-	if tiempo == str("18:47"):
-            GPIO.output(4, GPIO.HIGH)
-	    estado = 1
-            lcd.lcd_display_string("ALARMA",1)
-            lcd.lcd_display_string("ALARMA",2)
-            lcd.lcd_display_string("ALARMA",3)
-            lcd.lcd_display_string("ALARMA",4)
-            time.sleep(3)
-            lcd.lcd_clear()
+        #Variables para establecer el disparo del rele mediante una hora especifica 
+	#global tiempo
+        #global estado
+        #tiempo = time.strftime("%H:%M")
+	#if tiempo == str("18:47") :
+            #GPIO.output(4, GPIO.HIGH)
+	    #estado = 1
+            #lcd.lcd_display_string("ALARMA",1)
+            #lcd.lcd_display_string("ALARMA",2)
+            #lcd.lcd_display_string("ALARMA",3)
+            #lcd.lcd_display_string("ALARMA",4)
+            #time.sleep(3)
+            #lcd.lcd_clear()
         # Crea las variables para medir las presiones
         valores = [0]*2
         voltajes = [0]*2
@@ -97,7 +99,7 @@ def build_payload(variable_1, variable_2, variable_3, variable_4, variable_5):
 
         for i in range(2):
             valores[i] = adc.read_adc(i, gain=GAIN)
-            voltajes[i] = ((valores[i]*3.3)/2048)*1.37
+            voltajes[i] = ((valores[i]*3.3)/2048)*1.27
             corrientes[i] = ((voltajes[i]*0.0064)-0.00016)*1000
             presiones[i] = 0.3125*corrientes[i]-1.3
 
@@ -106,16 +108,25 @@ def build_payload(variable_1, variable_2, variable_3, variable_4, variable_5):
         global contador2
         global caudal1
         global caudal2
+	global ISRcontador1
+        global ISRcontador2
+	global reloj1
+	global tiempo1
 
         if contador1 != ISRcontador1:
             contador1 = ISRcontador1
-            a = contador1*100
-            caudal1 = a/30.
-
         if contador2 != ISRcontador2:
             contador2 = ISRcontador2
-            b = contador2*100
-            caudal2 = b/30.
+        # Cuenta las pulsaciones durante 180 segundos y luego calcula el caudal
+        reloj1 = int(time.strftime("%S"))
+        if reloj1 - tiempo1 > 30:
+            caudal1 = (contador1/30)*100
+            caudal2 = (contador2/30)*100
+            contador1 = 0
+            contador2 = 0
+            ISRcontador1 = 0
+	    ISRcontador2 = 0
+	    tiempo1 = reloj1
 
         # Escribe los datos adquiridos en el archivos de registro
         log.write("{0},{1},{2},{3},{4}\n".format(strftime("%d-%m-%Y %H:%M:%S"),str(caudal1),str(caudal2),str(presiones[0]),str(presiones[1])))
@@ -183,9 +194,7 @@ def post_request(payload):
 
 
 def main():
-    payload = build_payload(
-        VARIABLE_LABEL_1, VARIABLE_LABEL_2, VARIABLE_LABEL_3, VARIABLE_LABEL_4, VARIABLE_LABEL_5)
-
+    payload = build_payload(VARIABLE_LABEL_1, VARIABLE_LABEL_2, VARIABLE_LABEL_3, VARIABLE_LABEL_4, VARIABLE_LABEL_5)
     print("[INFO] Intentando enviar Datos")
     post_request(payload)
     print("[INFO] Listo!")
